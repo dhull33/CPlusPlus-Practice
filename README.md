21debcc8c61555f4ef1d03383f918ba44b363cac
David Hull <david.hull37@gmail.com>
2019-01-06 12:40:20 -0600

    Learning about types and arithmetic operators

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # C++ Practice
 #### Learning C++ through [PluralSight](https://app.pluralsight.com/library/courses/learn-programming-cplusplus)
+##### Using [CMake](https://cmake.org/) to compile
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // endl means end of line
+    cout << "Hello, World!" << endl << 2+2 ;
+
+    cout << endl << endl;
+
+    // declare an integer i and then initialize it with 3
+    int i;
+    i = 4;
+    cout << i << endl;
+
+    // When dividing, if both numbers are of type int the result will be an
+    // integer. Have to have at least one value as a float or double
+    double j = 7.0 / 2;
+    cout << j << endl;
+
+    // auto leaves the type up to the compiler
+    auto k = 45 / 3.5;
+    cout << k << endl;
     return 0;
 }
