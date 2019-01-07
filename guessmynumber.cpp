a5370e96317f03f83d68511a72d1101e2c7105f5
David Hull <david.hull37@gmail.com>
2019-01-07 12:28:47 -0600

    Completed the guessmynumber console app

--- guessmynumber.cpp
+++ guessmynumber.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main () {
-    int i;
-    int j;
     bool keepgoing = true;
+    int myanswer = 7;
+    int theiranswer;
+    while (keepgoing) {
+        cout << "Guess my number: ";
+        cin >> theiranswer;
+        if (theiranswer < myanswer) {
+            cout << "Your guess was to low. Please try again." << endl;
+        }
+        if (theiranswer > myanswer) {
+            cout <<"Your guess was to high. Please try again." << endl;
+        }
+        if (theiranswer == myanswer) {
+            cout << "You guessed the correct number!" << endl;
+            keepgoing = false;
+        }
+    }
 }
