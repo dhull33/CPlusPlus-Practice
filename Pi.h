e45c7f7443b58009367670337e55ef9bc96a2603
David Hull <david.hull37@gmail.com>
2020-05-19 14:55:59 -0500

    Added PI.C

---
+++ Pi.h
@@ -0,0 +1,8 @@
+//
+// Created by David Hull on 5/19/20.
+//
+
+#ifndef C__PRACTICE_PI_H
+#define C__PRACTICE_PI_H
+
+#endif //C__PRACTICE_PI_H
--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <string>
-#include "./headers/functions.h"
+// #include "./headers/functions.h"
 using namespace std;
 
 int main() {
-    print("Hello there!");
-    // endl means end of line
-    cout << "Hello, World!" << endl <<;
+    int a=10000, b = 0,c=2800,d, e = 0,f[2801],g;
 
-    // declare an integer i and then initialize it with 4
-    int i;
-    i = 4;
-    cout << i << endl;
+    for(;b-c;)
+        f[b++]=a/5;
+    for(;d=0, g=c*2; c-=14, cout << (e+d/a), e=d%a)
+        for(b=c; d+=f[b]*a, f[b]=d % --g, d/=g--,--b; d*=b);
 
-    // When dividing, if both numbers are of type int the result will be an
-    // integer. Have to have at least one value as a float or double
-    double j = 7.0 / 2;
-    cout << j << endl;
-
-    // auto leaves the type up to the compiler
-    auto k = 45 / 3.5;
-    cout << k << endl;
-    guessmynumber();
-    return 0;
+//    print("Hello there!");
+//    // endl means end of line
+//    cout << "Hello, World!" << endl;
+//
+//    // declare an integer i and then initialize it with 4
+//    int i;
+//    i = 4;
+//    cout << i << endl;
+//
+//    // When dividing, if both numbers are of type int the result will be an
+//    // integer. Have to have at least one value as a float or double
+//    double j = 7.0 / 2;
+//    cout << j << endl;
+//
+//    // auto leaves the type up to the compiler
+//    auto k = 45 / 3.5;
+//    cout << k << endl;
+//    guessmynumber();
+//    return 0;
 }
